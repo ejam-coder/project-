d5fde8b191301216f402f45fb4c86f8d40f793ec
ejam-coder <ejamescolar@gmail.com>
2025-12-13 22:04:09 -0600

    classify_rules.py

--- code/classify_rules.py
+++ code/classify_rules.py
@@ -1,1 +1,17 @@
+import numpy as np
 
+def classify_pose(landmarks):
+    """
+    Clasificación simple por reglas.
+    Retorna etiqueta de acción detectada.
+    """
+    LEFT_SHOULDER = 11
+    LEFT_WRIST = 15
+
+    shoulder = landmarks[LEFT_SHOULDER]
+    wrist = landmarks[LEFT_WRIST]
+
+    if wrist[1] < shoulder[1]:
+        return "HandUp"
+
+    return "Neutral"
